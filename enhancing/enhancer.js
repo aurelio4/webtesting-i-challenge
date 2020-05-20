e079bc8c48fa97278f857b5a7bb955259bde8db9
Aurelio Arcabascio <aurelioarcabascio@gmail.com>
2020-05-20 18:23:19 -0400

    finished project + stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -3,20 +3,63 @@ module.exports = {
   fail,
   repair,
   get,
-};
+}
 
 function succeed(item) {
-  return { ...item };
+  if (item.level === 20) {
+    item.level = item.level
+  } else {
+    item.level += 1
+  }
+
+  return {
+    ...item
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.level < 15) {
+    if (item.durability < 5) {
+      item.durability = 0
+    } else {
+      item.durability -= 5
+    }
+  }
+
+  if (item.level >= 15) {
+    if (item.durability < 10) {
+      item.durability = 0
+    } else {
+      item.durability -= 10
+    }
+  }
+
+  if (item.level > 16) {
+    item.level -= 1
+  }
+
+  return {
+    ...item
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100
+  return {
+    ...item
+  }
 }
 
 function get(item) {
-  return { ...item };
+  if (item.level === 0) {
+    return {
+      ...item
+    }
+  } else {
+    item.name = `[+${item.level}] ${item.name}`
+    item.level = 0
+    return {
+      ...item
+    }
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,67 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+let item
+const name = 'test item'
+const durability = 50
+const level = 17
+
+describe('item enhancer', () => {
+  it('should restore item durability to max', () => {
+
+    item = {
+      name,
+      durability,
+      level
+    }
+
+    expect(enhancer.repair(item)).toStrictEqual({
+      name,
+      durability: 100,
+      level
+    })
+  })
+
+  it('should upgrade item when the item successfully enhancement', () => {
+
+    item = {
+      name,
+      durability,
+      level
+    }
+
+    expect(enhancer.succeed(item)).toStrictEqual({
+      name: item.name,
+      durability: item.durability,
+      level: 18
+    })
+  })
+
+  it('should decrease item stats when the item successfully fails enhancement', () => {
+
+    item = {
+      name,
+      durability,
+      level
+    }
+
+    expect(enhancer.fail(item)).toStrictEqual({
+      name: item.name,
+      durability: 40,
+      level: 16
+    })
+  })
+
+  it('should change the name of the item based on its level', () => {
+    item = {
+      name,
+      durability,
+      level
+    }
+    expect(enhancer.get(item)).toStrictEqual({
+      name: '[+17] test item',
+      durability,
+      level: 0
+    })
+  })
+})
